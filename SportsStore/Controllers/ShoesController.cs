d5d5fcb5c0f87c8b6f1c7d60b1cd2383f75486f6
kerenhailu <keren4509@gmail.com>
2022-01-20 11:18:47 +0200

    Add project files.

---
+++ SportsStore/Controllers/ShoesController.cs
@@ -0,0 +1,27 @@
+﻿using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class ShoesController : Controller
+    {
+        StoreDbContextDataContext dbContext = new StoreDbContextDataContext();
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllTheShoes()
+        {
+            List<Shoe> listShoes= dbContext.Shoes.ToList();
+            ViewBag.Shoes = listShoes;
+            ViewBag.button = true;
+            return View();
+        }
+
+    }
+}
